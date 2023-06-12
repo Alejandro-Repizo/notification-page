f4b896ce0ac20d087eab39e805a94f2f251a6b84
Alejandro-Repizo <alejo.oafr2001@gmail.com>
2023-06-12 17:00:40 -0500

    feat(root) Added deploy to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        server: {
-            watch: {
-                usePolling: true
-            }
-        }
+  vite: {
+    server: {
+      watch: {
+        usePolling: true
+      }
     }
+  },
+  output: "server",
+  adapter: netlify()
 });
